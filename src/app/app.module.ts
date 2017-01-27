ca2d505adecbe765d41bb64e38689946748550fe
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-26 18:25:50 -0800

    add about page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
